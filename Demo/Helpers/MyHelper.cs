857793c6158ec371a03b687541939f2ebf1f9213
Michael Naumov <mnaoumov@gmail.com>
2015-06-20 10:48:42 +0300

    Add Demo project

---
+++ Demo/Helpers/MyHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Helpers
+{
+    public static class MyHelper
+    {
+        public static string Greet(string name)
+        {
+            return string.Format("Hello {0}!", name);
+        }
+    }
+}
